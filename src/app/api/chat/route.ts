de0397060b61cfd04dcb00253885b2a54e25463c
Igzanotto <ignaciozanotto@gmail.com>
2024-04-15 15:31:45 -0300

    dog doctor

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,15 @@
 import {Configuration, OpenAIApi} from 'openai-edge'
+import { NextResponse } from 'next/server'
 
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
     })
 
-    const openai = new OpenAIApi(config)
+const openai = new OpenAIApi(config)
+
+export async function POST(request: Request) {
+    const {messages} = await request.json()
+    console.log(messages)
+
+    return NextResponse.json({message: "respuesta del POST"})
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { useChat } from "ai/react";
+
 function HomePage() {
+
+  const { handleInputChange, handleSubmit } = useChat();
+
   return (
     <section className="flex justify-center items-center h-screen">
-    <form className="max-w-xl w-full">
+      <form className="max-w-xl w-full" onSubmit={handleSubmit}>
         <div className="flex justify-between my-4">
-          <label className="block font-bold my-2">
-            Say something...
-          </label>
+          <label className="block font-bold my-2">Say something...</label>
           <button className="bg-blue-600 text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:opacity-50">
             Send
           </button>
@@ -15,6 +18,7 @@ function HomePage() {
           className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
           placeholder="Type something..."
           autoFocus
+          onChange={handleInputChange}
         />
       </form>
     </section>
