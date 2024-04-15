4bc5da532594bbe0971d3483ae09117889c16a00
Igzanotto <ignaciozanotto@gmail.com>
2024-04-15 20:16:21 -0300

    use client

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import {Configuration, OpenAIApi} from 'openai-edge'
 import { NextResponse } from 'next/server'
+import {OpenAIStream} from 'ai'
 
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -10,6 +11,14 @@ const openai = new OpenAIApi(config)
 export async function POST(request: Request) {
     const {messages} = await request.json()
     console.log(messages)
+
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        stream: true,
+        messages: messages
+    })
+
+    const stream = OpenAIStream(response)
 
     return NextResponse.json({message: "respuesta del POST"})
 }
