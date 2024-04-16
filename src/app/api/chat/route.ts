f1f416e5bc148c8ff2ee508fc30014b9f366a2e6
Igzanotto <ignaciozanotto@gmail.com>
2024-04-15 22:04:48 -0300

    mid

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import {Configuration, OpenAIApi} from 'openai-edge'
 import { NextResponse } from 'next/server'
-import {OpenAIStream} from 'ai'
+import {OpenAIStream,StreamingTextResponse} from 'ai'
 
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -16,9 +16,11 @@ export async function POST(request: Request) {
         model: "gpt-3.5-turbo",
         stream: true,
         messages: messages
+        // messages: [ {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        // {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}],
     })
 
     const stream = OpenAIStream(response)
 
-    return NextResponse.json({message: "respuesta del POST"})
+    return new StreamingTextResponse(stream)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
-"use client"
+"use client";
 
 import { useChat } from "ai/react";
 
 function HomePage() {
+  const { handleInputChange, handleSubmit, messages } = useChat();
 
-  const { handleInputChange, handleSubmit } = useChat();
+  console.log(messages);
 
   return (
     <section className="flex justify-center items-center h-screen">
       <form className="max-w-xl w-full" onSubmit={handleSubmit}>
+        {messages.map((message) => (
+          <div key={message.id} className="flex justify-between my-4">
+            <div className="flex flex-col">
+              <span className="font-bold text-gray-700">{message.role}</span>
+              <span className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600">
+                {message.content}
+              </span>
+            </div>
+          </div>
+        ))}
+
         <div className="flex justify-between my-4">
           <label className="block font-bold my-2">Say something...</label>
           <button className="bg-blue-600 text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:opacity-50">
